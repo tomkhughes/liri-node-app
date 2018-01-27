47d5567a35825d1a079f5203bddbd11db520f21c
Tom Hughes <tomkhughes@g.cofc.edu>
2018-01-26 19:25:12 -0800

    finished

--- liri.js
+++ liri.js
@@ -15,17 +15,17 @@ function switchCase() {
             spotifySong();
             break;
 
-            case 'movie-this': 
+        case 'movie-this':
             movieThis();
             break;
 
-            case 'do-what-it-says': 
+        case 'do-what-it-says':
             doWhatItSays();
             break;
-            default:
-                  {
-                      console.log('invalid input!!!')
-                  }
+        default:
+            {
+                console.log('invalid input!!!')
+            }
     }
 }
 
@@ -43,12 +43,12 @@ function myTweets() {
         access_token_secret: 'W6iipMO14adAL8jlY50tet6COIzQiShFjZxcjoa5FJ2iV'
     });
 
-    var params = { screen_name: 'tomkhughes' };
+    var params = { screen_name: 'kingjames' };
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
             console.log(error);
         }
-        for(var i = 0; i < tweets.length; i++) {
+        for (var i = 0; i < tweets.length; i++) {
             console.log(tweets[i].text);
             console.log(tweets[i].created_at);
 
@@ -87,48 +87,49 @@ function spotifySong() {
 
 };
 
-function movieThis(){
+function movieThis() {
     if (input == null) {
         input = 'Mr.Nobody';
     };
     var queryUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=de2ecd";
 
 
-// This line is just to help us debug against the actual URL.
-console.log(queryUrl);
+    // This line is just to help us debug against the actual URL.
+    console.log(queryUrl);
 
 
-// Then create a request to the queryUrl
+    // Then create a request to the queryUrl
 
-// ...
-var request = require("request");
+    // ...
+    var request = require("request");
 
-// Then run a request to the OMDB API with the movie specified
-request(queryUrl, function(error, response, data) {
+    // Then run a request to the OMDB API with the movie specified
+    request(queryUrl, function(error, response, data) {
 
-  // If the request is successful (i.e. if the response status code is 200)
-  if (!error && response.statusCode === 200) {
+        // If the request is successful (i.e. if the response status code is 200)
+        if (!error && response.statusCode === 200) {
 
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    // console.log(JSON.parse(data));
-    console.log("Title: " + JSON.parse(data).Title);
-    console.log("Release Year: " + JSON.parse(data).Year);
-    console.log("IMBD Rating: " + JSON.parse(data).imdbRating);
-    console.log("Rotten Tomatoes Rating: " + JSON.parse(data).tomatoRating);
-    console.log("Country: " + JSON.parse(data).Country);
-    console.log("Language: " + JSON.parse(data).Language);
-    console.log("Plot: " + JSON.parse(data).Plot);
-    console.log("Actors: " + JSON.parse(data).Actors);
+            // Parse the body of the site and recover just the imdbRating
+            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+            // console.log(JSON.parse(data));
+            console.log("Title: " + JSON.parse(data).Title);
+            console.log("Release Year: " + JSON.parse(data).Year);
+            console.log("IMBD Rating: " + JSON.parse(data).imdbRating);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(data).tomatoRating);
+            console.log("Country: " + JSON.parse(data).Country);
+            console.log("Language: " + JSON.parse(data).Language);
+            console.log("Plot: " + JSON.parse(data).Plot);
+            console.log("Actors: " + JSON.parse(data).Actors);
 
-  }
+        }
 
-});
+    });
 }
-function doWhatItSays(){
+
+function doWhatItSays() {
     const fs = require('fs');
     fs.readFile('random.txt', 'utf8', function(err, data) {
-        if (err) {throw err}
+        if (err) { throw err }
         const randomText = data.split(', ');
         console.log(randomText);
         command = randomText[0]
